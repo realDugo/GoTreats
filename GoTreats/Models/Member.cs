3b2107e02ad054e5f4ae657369d3cc7ff1be2bfa
realDugo <n3llsworth@yahoo.com>
2014-09-18 23:05:34 -0700

    add new properties to Members model

---
+++ GoTreats/Models/Member.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GoTreats.Models
+{
+    public class Member
+    {
+        public int MemberId { get; set; }
+        [Required()]
+        [DataType(DataType.Text)]
+        public string FirstName { get; set; }
+        [Required()]
+        [DataType(DataType.Text)]
+        public string LastName { get; set; }
+        [DataType(DataType.Text)]
+        public string UserName { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime StartDate { get; set; }
+        [Required()]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        public DateTime LastTreatDate { get; set; }
+        public DateTime NextTreatDate { get; set; }
+
+    }
+}
---
+++ GoTreats/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GoTreats.Startup))]
+namespace GoTreats
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
